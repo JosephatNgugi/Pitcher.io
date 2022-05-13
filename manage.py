ee3da7b4396f316e12af0c222acb67d46ca741c6
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-13 23:18:06 +0300

    define tests for user functions

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from flask_script import Manager, Server
 from app import create_app
+from app.models import Comment, Pitch, User, Vote
 
 app = create_app('development')
 
@@ -9,7 +10,15 @@ manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, User=User,Pitch=Pitch, Comment=Comment, Vote=Vote)
+
+@manager.command
+def test():
+    """Run the unit tests"""
+    import unittest
+
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__=="__main__":
     manager.run()
