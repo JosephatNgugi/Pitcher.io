31aead4e116d69a56d401e43f4498bb78a871f5b
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-14 23:42:04 +0300

    Define a login route and display the login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,17 @@
-from flask import render_template
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
+from .forms import LoginForm
+from ..models import User
+from flask_login import login_user
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
-    title = 'Login'
-    
-    return render_template("auth/login.html", title=title)
+    form = LoginForm()
+    if form.validate_on_submit:
+        user = User.query.filter_by(email = form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+    flash('Invalid username or Password')
+    title = "User Login"
+    return render_template('auth/login.html', form=form, title=title)
