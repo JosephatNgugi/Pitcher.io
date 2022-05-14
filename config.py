93efeb5b5c0ae378a20613aea8fa45e1fb01a53f
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-14 22:34:06 +0300

    update config and pitch model class

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Elm1n10@localhost/pitches'
-    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 class ProdConfig(Config):
     """
     Config Child class for Production configurations 
