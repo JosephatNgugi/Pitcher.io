6845054a3439617fbbf6b6eb0cb9ce1d802d87f8
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-14 09:35:05 +0300

    setup flask_migrate to handle migrations

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from flask_script import Manager, Server
-from app import create_app
+from app import create_app,db
+from flask_migrate import Migrate, MigrateCommand
 from app.models import Comment, Pitch, User, Vote
 
 app = create_app('development')
@@ -10,7 +11,10 @@ manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, User=User,Pitch=Pitch, Comment=Comment, Vote=Vote)
+    return dict(app=app, db=db, User=User,Pitch=Pitch, Comment=Comment, Vote=Vote)
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
