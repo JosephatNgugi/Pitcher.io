b0bb46c78ecb8d17f54b528477c00da65134281b
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-15 00:15:37 +0300

    Create a signup route and a mail messaging option to user upon sign up. cdefine a logout function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template,redirect,url_for,flash,request
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from ..models import User
-from flask_login import login_user
+from flask_login import login_required, login_user, logout_user
+from ..email import mail_message
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
@@ -15,3 +16,20 @@ def login():
     flash('Invalid username or Password')
     title = "User Login"
     return render_template('auth/login.html', form=form, title=title)
+
+@auth.route('/signup',methods = ['GET','POST'])
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,username = form.username.data,password=form.password.data)
+        user.save_user()
+        mail_message("Welcome to Pitcher","email/welcome_user", user.email,user=user)
+        return redirect(url_for('auth.login'))
+    title = 'Sign Up'
+    return render_template('auth/signup.html',title=title, reg_form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
