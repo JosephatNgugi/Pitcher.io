032a9d19ad05f2c8c166b1b056430999f283e3f7
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-15 09:43:59 +0300

    add email configurations

--- config.py
+++ config.py
@@ -6,6 +6,13 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Elm1n10@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 class ProdConfig(Config):
     """
     Config Child class for Production configurations 
