3c405cd321752b60f7774c57d16e802fffcc16aa
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-16 14:39:06 +0300

    10th Heroku deployment

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     """Parent class for general configurations settings"""
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Elm1n10@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Elm1n10@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
